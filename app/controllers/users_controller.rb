f1911c94e13c3a17c8e4430f0d7ae9de0ba319a5
thealexcesar <thealexcesar@gmail.com>
2022-01-13 16:30:47 -0300

    bootstrap

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def index
     @users = User.all
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: "Post was successfully created by controller." }
+      end
+    end
+  end
+
+  private
+    def user_params
+      params.require(:user).permit(:title, :body, :name)
+    end
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,20 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+#
+
+pass = "123456"
+User.create![
+  {
+    name: "alex", email: "thealexcesar@gmail.com",
+    password: pass, password_confirmation: pass, type: "admin"
+  },
+  {
+    name: "son goku", email: "kakaroto@gmail.com",
+    password: pass, password_confirmation: pass, type: "user"
+  },
+  {
+    name: "Naruto Usumaki", email: "kurama@email.com",
+    branch: "Ttajaí", infraction: 495574, type: "user"
+  },
+]
