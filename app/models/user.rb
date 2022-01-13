bd9d1a58641563222e3e928ac221b1c5145075d7
thealexcesar <thealexcesar@gmail.com>
2022-01-13 14:32:05 -0300

    adjust type migrations

--- app/models/user.rb
+++ app/models/user.rb
@@ -3,5 +3,5 @@ class User < ApplicationRecord
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
-  enum level: [:admin, :common]
+  enum user_type: [:user, :admin]
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,4 @@
 Rails.application.routes.draw do
-  devise_for :users
   root to: "users#index"
+  devise_for :users
 end
